38e431e83e3afaea8802d123dec488bd050b5cda
Leonard Brünings <leonard.bruenings@gradle.com>
2021-12-17 11:01:24 +0100

    Apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate google_calendar3 as calendar3;
 extern crate hyper;
 extern crate hyper_rustls;
 extern crate itertools;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate regex;
 extern crate time;
 extern crate yup_oauth2 as oauth2;
@@ -21,17 +22,17 @@ use clap::App;
 use clap::Arg;
 use clap::ArgMatches;
 use clap::SubCommand;
-use hyper::Client;
 use hyper::net::HttpsConnector;
+use hyper::Client;
 use itertools::Itertools;
-use oauth2::{Authenticator, DefaultAuthenticatorDelegate};
+use oauth2::read_application_secret;
 use oauth2::AuthenticatorDelegate;
 use oauth2::ConsoleApplicationSecret;
 use oauth2::DiskTokenStorage;
 use oauth2::FlowType;
 use oauth2::GetToken;
-use oauth2::read_application_secret;
 use oauth2::Token;
+use oauth2::{Authenticator, DefaultAuthenticatorDelegate};
 use regex::Regex;
 use time::Duration;
 
@@ -39,7 +40,7 @@ fn main() {
     let cli_args = &App::new("Zoom Alfred Workflow")
         .subcommand(
             SubCommand::with_name("code")
-                .arg(Arg::with_name("code").help("The OAuth 2.0 verification code from Google."))
+                .arg(Arg::with_name("code").help("The OAuth 2.0 verification code from Google.")),
         )
         .arg(Arg::with_name("search").multiple(true))
         .get_matches();
@@ -60,12 +61,16 @@ fn main() {
                 .subtitle("Follow this guide on creating client credentials")
                 .arg("https://developers.google.com/youtube/registering_an_application#Create_OAuth2_Tokens")
                 .into_item());
-            items.push(alfred::ItemBuilder::new("Or go to the Google Developer Console directly")
-                .arg("https://console.developers.google.com/apis/credentials")
-                .into_item());
-            items.push(alfred::ItemBuilder::new("Finally, download the json file")
-                .subtitle("to ~/.zoom-alfred-workflow/client_secret.json")
-                .into_item());
+            items.push(
+                alfred::ItemBuilder::new("Or go to the Google Developer Console directly")
+                    .arg("https://console.developers.google.com/apis/credentials")
+                    .into_item(),
+            );
+            items.push(
+                alfred::ItemBuilder::new("Finally, download the json file")
+                    .subtitle("to ~/.zoom-alfred-workflow/client_secret.json")
+                    .into_item(),
+            );
         }
         Ok(secret) => {
             if !token_path.exists() {
@@ -73,42 +78,40 @@ fn main() {
                 items.extend(permission_flow(action, secret, token_path_string));
             } else {
                 let search = if let Some(matches) = cli_args.values_of("search") {
-                    matches.into_iter().join(" " )
+                    matches.into_iter().join(" ")
                 } else {
                     String::new()
                 };
                 items.extend(main_flow(secret, &token_path_string, search))
             }
-        },
+        }
     };
-
 
     alfred::json::write_items(io::stdout(), items.as_slice()).unwrap();
 }
 
-
 fn determine_permission_action(matches: &ArgMatches) -> PermissionAction {
     if let Some(matches) = matches.subcommand_matches("code") {
         if let Some(code) = matches.value_of("code") {
-            return PermissionAction::UserEnteredCode(String::from(code))
+            return PermissionAction::UserEnteredCode(String::from(code));
         } else {
-            return PermissionAction::UserShouldEnterCode
+            return PermissionAction::UserShouldEnterCode;
         }
     }
-    return PermissionAction::ShowVerificationURL
+    return PermissionAction::ShowVerificationURL;
 }
 
 enum PermissionAction {
     ShowVerificationURL,
     UserShouldEnterCode,
-    UserEnteredCode(String)
+    UserEnteredCode(String),
 }
 
 fn read_secret(path: &Path) -> io::Result<ConsoleApplicationSecret> {
     let secret = read_application_secret(path)?;
     Ok(ConsoleApplicationSecret {
         web: None,
-        installed: Some(secret)
+        installed: Some(secret),
     })
 }
 
@@ -118,21 +121,27 @@ fn new_client() -> Client {
 
 fn extract_zoom_link(txt: String) -> Option<String> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"(https?://(.*?zoom\.us)/j/([0-9]+))(?:\?pwd=(\w+))?").unwrap();
+        static ref RE: Regex =
+            Regex::new(r"(https?://(.*?zoom\.us)/j/([0-9]+))(?:\?pwd=(\w+))?").unwrap();
     }
 
-    let n = RE.captures(txt.as_str()).iter().next().map(|c|
-        format!("zoommtg://{}/join?action=join&confno={}{}", c.get(2).unwrap().as_str(), c.get(3).unwrap().as_str(), c.get(4).map_or("".to_string(), |m| format!("&pwd={}", m.as_str())))
-    );
+    let n = RE.captures(txt.as_str()).iter().next().map(|c| {
+        format!(
+            "zoommtg://{}/join?action=join&confno={}{}",
+            c.get(2).unwrap().as_str(),
+            c.get(3).unwrap().as_str(),
+            c.get(4)
+                .map_or("".to_string(), |m| format!("&pwd={}", m.as_str()))
+        )
+    });
 
     return n;
 }
 
-
 struct UrlExtractingAuthenticatorDelegate<'a> {
-    verification_url : &'a mut String
+    verification_url: &'a mut String,
 }
-impl <'a> AuthenticatorDelegate for UrlExtractingAuthenticatorDelegate<'a> {
+impl<'a> AuthenticatorDelegate for UrlExtractingAuthenticatorDelegate<'a> {
     fn present_user_url(&mut self, url: &String, _need_code: bool) -> Option<String> {
         self.verification_url.push_str(url.as_str());
         None
@@ -140,7 +149,7 @@ impl <'a> AuthenticatorDelegate for UrlExtractingAuthenticatorDelegate<'a> {
 }
 
 struct UserProvidedCodeAuthenticatorDelegate {
-    code: String
+    code: String,
 }
 impl AuthenticatorDelegate for UserProvidedCodeAuthenticatorDelegate {
     fn present_user_url(&mut self, _url: &String, _need_code: bool) -> Option<String> {
@@ -152,99 +161,160 @@ fn get_verification_url(secret: ConsoleApplicationSecret, token_file: &String) -
     let mut urdel = String::from("");
 
     let delegate = UrlExtractingAuthenticatorDelegate {
-        verification_url : &mut urdel
+        verification_url: &mut urdel,
     };
 
-
-    let mut alfred_auth =
-        Authenticator::new(&secret.installed.unwrap(), delegate,
-                           new_client(), DiskTokenStorage::new(token_file).unwrap(), Some(FlowType::InstalledInteractive));
+    let mut alfred_auth = Authenticator::new(
+        &secret.installed.unwrap(),
+        delegate,
+        new_client(),
+        DiskTokenStorage::new(token_file).unwrap(),
+        Some(FlowType::InstalledInteractive),
+    );
     let token = alfred_auth.token(&["https://www.googleapis.com/auth/calendar.events.readonly"]);
     assert_eq!(token.is_err(), true);
     return urdel;
 }
 
-fn verify_code(secret: ConsoleApplicationSecret, token_file: &String, verification_code: &String) -> Result<Token, Box<Error>> {
+fn verify_code(
+    secret: ConsoleApplicationSecret,
+    token_file: &String,
+    verification_code: &String,
+) -> Result<Token, Box<Error>> {
     let delegate = UserProvidedCodeAuthenticatorDelegate {
-        code : verification_code.clone()
+        code: verification_code.clone(),
     };
-    let mut alfred_auth =
-        Authenticator::new(&secret.installed.unwrap(), delegate,
-                           new_client(), DiskTokenStorage::new(token_file).unwrap(), Some(FlowType::InstalledInteractive));
+    let mut alfred_auth = Authenticator::new(
+        &secret.installed.unwrap(),
+        delegate,
+        new_client(),
+        DiskTokenStorage::new(token_file).unwrap(),
+        Some(FlowType::InstalledInteractive),
+    );
     let token = alfred_auth.token(&["https://www.googleapis.com/auth/calendar.events.readonly"]);
-    return token
+    return token;
 }
 
-fn permission_flow(action: PermissionAction, secret: ConsoleApplicationSecret, token_file: &String) -> Vec<alfred::Item> {
+fn permission_flow(
+    action: PermissionAction,
+    secret: ConsoleApplicationSecret,
+    token_file: &String,
+) -> Vec<alfred::Item> {
     let mut v = Vec::new();
     match action {
         PermissionAction::ShowVerificationURL => {
             let url = get_verification_url(secret, token_file);
-            v.push(alfred::ItemBuilder::new("1. Open Google authentication page").valid(true).arg(url).into_item());
-            v.push(alfred::ItemBuilder::new("2. Enter Verification Code").valid(false).autocomplete("code ").into_item());
-        },
+            v.push(
+                alfred::ItemBuilder::new("1. Open Google authentication page")
+                    .valid(true)
+                    .arg(url)
+                    .into_item(),
+            );
+            v.push(
+                alfred::ItemBuilder::new("2. Enter Verification Code")
+                    .valid(false)
+                    .autocomplete("code ")
+                    .into_item(),
+            );
+        }
         PermissionAction::UserShouldEnterCode => {
-            v.push(alfred::ItemBuilder::new("Paste the Verification Code").valid(false).into_item());
-        },
-        PermissionAction::UserEnteredCode(code) => {
-            match verify_code(secret, token_file, &code) {
-                Ok(_) => {
-                    v.push(alfred::ItemBuilder::new("Tokens Stored Successfully").valid(false).into_item())
-                },
-                Err(error) => {
-                    v.push(alfred::ItemBuilder::new(String::from(error.to_string().as_str()) + " :: " + code.as_str()).into_item())
-                },
-            }
+            v.push(
+                alfred::ItemBuilder::new("Paste the Verification Code")
+                    .valid(false)
+                    .into_item(),
+            );
+        }
+        PermissionAction::UserEnteredCode(code) => match verify_code(secret, token_file, &code) {
+            Ok(_) => v.push(
+                alfred::ItemBuilder::new("Tokens Stored Successfully")
+                    .valid(false)
+                    .into_item(),
+            ),
+            Err(error) => v.push(
+                alfred::ItemBuilder::new(
+                    String::from(error.to_string().as_str()) + " :: " + code.as_str(),
+                )
+                .into_item(),
+            ),
         },
     }
     v
 }
 
-fn main_flow(secret: ConsoleApplicationSecret, token_file: &String, search: String) -> Vec<alfred::Item> {
-    let auth = Authenticator::new(&secret.installed.unwrap(), DefaultAuthenticatorDelegate,
-                                  new_client(), DiskTokenStorage::new(token_file).unwrap(), Some(FlowType::InstalledInteractive));
+fn main_flow(
+    secret: ConsoleApplicationSecret,
+    token_file: &String,
+    search: String,
+) -> Vec<alfred::Item> {
+    let auth = Authenticator::new(
+        &secret.installed.unwrap(),
+        DefaultAuthenticatorDelegate,
+        new_client(),
+        DiskTokenStorage::new(token_file).unwrap(),
+        Some(FlowType::InstalledInteractive),
+    );
 
     let hub = CalendarHub::new(new_client(), auth);
 
     let now = Utc::now();
     let tomorrow_midnight = (Utc::today() + Duration::days(2)).and_hms(0, 0, 0);
 
-    let (_, events) = hub.events()
+    let (_, events) = hub
+        .events()
         .list("primary")
         .single_events(true)
         .time_min(now.format("%+").to_string().as_str())
         .time_max(tomorrow_midnight.format("%+").to_string().as_str())
         .order_by("starttime")
-        .doit().unwrap();
+        .doit()
+        .unwrap();
 
     let mut items: Vec<alfred::Item> = Vec::new();
 
     for e in events.items.unwrap() {
-        let start = e.start.and_then(|dt| dt.date_time).and_then(|dt| DateTime::parse_from_rfc3339(dt.as_str()).ok());
+        let start = e
+            .start
+            .and_then(|dt| dt.date_time)
+            .and_then(|dt| DateTime::parse_from_rfc3339(dt.as_str()).ok());
         let creator = e.creator.and_then(|c| c.display_name.or(c.email));
 
-        let meeting_code: Option<String> = e.conference_data.into_iter().flat_map(|c|
-            c.entry_points.into_iter()
-                .flat_map(|e| e.into_iter()))
+        let meeting_code: Option<String> = e
+            .conference_data
+            .into_iter()
+            .flat_map(|c| c.entry_points.into_iter().flat_map(|e| e.into_iter()))
             .find_map(|e| e.uri.and_then(|u| extract_zoom_link(u)));
 
-
-        let zoom = e.description.and_then(|d| extract_zoom_link(d))
+        let zoom = e
+            .description
+            .and_then(|d| extract_zoom_link(d))
             .or(e.location.and_then(|l| extract_zoom_link(l)))
             .or(meeting_code);
 
         let summary = e.summary.unwrap();
         if zoom.is_some() && (search.is_empty() || summary.contains(search.as_str())) {
-            let today_or_tomorrow = if start.unwrap().day() == Utc::today().day() { "Today" } else { "Tomorrow" };
-            let item = alfred::ItemBuilder::new(format!("{} - {} at {}", summary, today_or_tomorrow, start.unwrap().time().format("%H:%M")))
-                .subtitle(creator.unwrap())
-                .arg(zoom.unwrap())
-                .into_item();
+            let today_or_tomorrow = if start.unwrap().day() == Utc::today().day() {
+                "Today"
+            } else {
+                "Tomorrow"
+            };
+            let item = alfred::ItemBuilder::new(format!(
+                "{} - {} at {}",
+                summary,
+                today_or_tomorrow,
+                start.unwrap().time().format("%H:%M")
+            ))
+            .subtitle(creator.unwrap())
+            .arg(zoom.unwrap())
+            .into_item();
             items.push(item);
         }
     }
     if items.is_empty() {
-        items.push(alfred::ItemBuilder::new("No Zoom meeting scheduled in the next 24 hours.").valid(false).into_item())
+        items.push(
+            alfred::ItemBuilder::new("No Zoom meeting scheduled in the next 24 hours.")
+                .valid(false)
+                .into_item(),
+        )
     }
 
     items
